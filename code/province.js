28b0c13e13290f067052afb7bd6786638086cfc2
twesix <278227739@qq.com>
2018-04-01 22:39:55 +0800

    人数分布完成

--- code/province.js
+++ code/province.js
@@ -1,7 +1,7 @@
 const mongodb = require('./mongodb')
 const fs = require('fs')
 const regions = JSON.parse(fs.readFileSync('./region.json').toString())
-console.log(regions)
+const path = require('path')
 
 const $conn = mongodb.$conn
 
@@ -10,6 +10,19 @@ const $conn = mongodb.$conn
     const db = await $conn
     const user = db.collection('user')
 
-
+    const result = {}
+    for(const gb in regions)
+    {
+        const num = await user.find
+        (
+            {
+                'profile.province': Number(gb)
+            }
+        ).count()
+        console.log(`| ${regions[gb]} | ${num} |`)
+        result[regions[gb]] = num
+    }
+    // console.log(result)
+    fs.writeFileSync(path.resolve(__dirname, 'province.json'), JSON.stringify(result))
 })()
 
