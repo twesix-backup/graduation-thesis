5605c252d07022c14f5417403027ceaa21bdd304
twesix <278227739@qq.com>
2018-04-09 09:19:32 +0800

    改进处理程序

--- code/follow/sex.js
+++ code/follow/sex.js
@@ -9,68 +9,67 @@ const $conn = mongodb.$conn
     const $follow = $db.collection('follow')
     const $follow_sex = $db.collection('follow_sex')
 
-    const cursor = $user.find
-    (
-        {
-            'profile.gender':
-                {
-                    $in: [1, 2]
-                },
-            '__processed.follow': true
-        }
-    )
-    const skip = 1875
-    cursor.skip(skip)
-    // console.log(cursor)
-    console.log(`find ${await cursor.count()} users`)
-    let num = skip
-    while(await cursor.hasNext())
+    let skip = 7133
+    process.on('uncaughtException', async function(e)
     {
-        console.log(num ++)
-        const _user = await cursor.next()
+        console.log(e)
+        skip --
+        console.log(`start from: ${skip}`)
+        await start(skip)
+    })
 
-        const cursor1 = $follow.find
+    async function start()
+    {
+        const userCursor = $user.find
         (
             {
-                from: _user.profile.userId
+                'profile.gender':
+                    {
+                        $in: [1, 2]
+                    },
+                '__processed.follow': true
             }
         )
-        console.log(`find ${await cursor1.count()} follow record from user ${_user.profile.userId}`)
-        while(await cursor1.hasNext())
+        userCursor.skip(skip)
+
+        console.log(`find ${await userCursor.count()} users`)
+
+        while(await userCursor.hasNext())
         {
-            const _follow = await cursor1.next()
-            const _to = await $user.findOne({'profile.userId': _follow.to})
-            // console.log(_to)
+            console.log(skip ++)
+            const user = await userCursor.next()
 
-            if( ! _to)
-            {
-                console.log(`user ${_follow.to} has no profile`)
-                continue
-            }
-
-            console.log(`insert one follow record: ${_user.profile.userId} --> ${_to.profile.userId}`)
-            const record =
-                {
-                    from: _user.profile,
-                    to: _to.profile
-                }
-            await $follow_sex.updateOne
+            const followCursor = $follow.find
             (
-                record,
                 {
-                    $set:
-                        {
-                            forAnalyse: true
-                        }
-                },
-                {
-                    upsert: true
+                    from: user.profile.userId
                 }
             )
+            console.log(`find ${await followCursor.count()} follow record from user ${user.profile.userId}`)
+            const records = []
+            while(await followCursor.hasNext())
+            {
+                const followRecord = await followCursor.next()
+                const to = await $user.findOne({'profile.userId': followRecord.to})
 
-            // break
+                if( ! to)
+                {
+                    console.log(`user ${followRecord.to} has no profile`)
+                    continue
+                }
+
+                console.log(`insert one follow record: ${user.profile.userId} --> ${to.profile.userId}`)
+                const record =
+                    {
+                        from: user.profile,
+                        to: to.profile
+                    }
+
+                records.push(record)
+            }
+            await $follow_sex.insert(records)
         }
-
-        // break
     }
+
+    await start()
 })()
