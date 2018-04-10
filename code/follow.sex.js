41a5bf858824bbe953fa09fd09a92ff5bc2e9714
twesix <278227739@qq.com>
2018-04-10 10:17:58 +0800

    男女互相关注数量, 有问题待修复

---
+++ code/follow.sex.js
@@ -0,0 +1,46 @@
+const mongodb = require('./mongodb')
+
+const $conn = mongodb.$conn
+
+;(async function()
+{
+    const $db = await $conn
+    const $follow_sex = $db.collection('follow_sex')
+
+    const male2male = await $follow_sex.find
+    (
+        {
+            'from.gender': 1,
+            'to.gender': 1,
+        }
+    ).count()
+    const male2female = await $follow_sex.find
+    (
+        {
+            'from.gender': 1,
+            'to.gender': 2,
+        }
+    ).count()
+    const female2male = await $follow_sex.find
+    (
+        {
+            'from.gender': 2,
+            'to.gender': 1,
+        }
+    ).count()
+    const female2female = await $follow_sex.find
+    (
+        {
+            'from.gender': 2,
+            'to.gender': 2,
+        }
+    ).count()
+
+    const matrix =
+        [
+            [male2male, male2female],
+            [female2male, female2female]
+        ]
+    console.log(matrix)
+})()
+
